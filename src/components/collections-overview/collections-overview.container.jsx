258928fdd0df47ca3426620f8f1f139acdd61d58
ceafive <ceafive@gmail.com>
2020-06-17 00:13:41 +0000

    styled components done

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -1,9 +1,18 @@
-import React from "react";
-import moduleName from "react-redux";
+import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import { compose } from "redux";
 
 import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
-import moduleName from "../with-spinner/with-spinner.component";
+import WithSpinner from "../with-spinner/with-spinner.component";
+import CollectionsOverview from "./collections-overview.component";
+
 const mapStateToProps = createStructuredSelector({
   isLoading: selectIsCollectionFetching,
 });
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
